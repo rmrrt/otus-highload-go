fd127082bafa758605a5cdff2778da273d8ea3fc
rmrrt <remarrte@gmail.com>
2024-03-04 20:59:33 +0400

    add register endpoiint

--- app/main.go
+++ app/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
+
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Birthday  string `json:"birthday"`
+	City      string `json:"city"`
+	Interests string `json:"interests"`
+	Sex       string `json:"sex"`
+}
 
 func main() {
 	db, err := sql.Open("postgres", "host=localhost port=5432 user=youruser dbname=yourdb password=yourpassword sslmode=disable")
@@ -19,6 +30,19 @@ func main() {
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "Something"})
+	})
+
+	r.POST("/user/register", func(c *gin.Context) {
+
+		var user User
+
+		if c.ShouldBind(&user) != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "WRONG JSON"})
+			log.Print(&user)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "OKAY"})
 	})
 
 	r.Run()
